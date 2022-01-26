0e80856ce117fbcefdcb8e739d8de3977eef66e9
Fathur Walkers <fathurwalkers44@gmail.com>
2022-01-26 04:22:55 -0800

    update

---
+++ app/Controllers/Dashboard.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controllers;
+
+class Dashboard extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+
+    public function logout()
+    {
+        $session = session();
+        $session->stop();
+        return redirect()->to('/login');
+    }
+
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function register()
+    {
+        return view('register');
+    }
+
+    public function postregister()
+    {
+        $this->usermodel->save([
+            'username' => $this->request->getVar('username'),
+            'password' => $this->request->getVar('password'),
+            'level' => "admin",
+        ]);
+        return redirect()->to('/login');
+    }
+
+    public function postlogin()
+    {
+        $session = session();
+
+        $username = $this->request->getPost('username');
+        $password = $this->request->getPost('password');
+
+        $cocokuser = $this->usermodel->where('username', $username)->first();
+        if ($cocokuser) {
+            $cocokpassword = $this->usermodel->where('password', $password)->first();
+            if ($cocokpassword) {
+                $setData = [
+                    'username' => $username,
+                    'password' => $password
+                ];
+                $session->set($setData);
+                return redirect()->to('/dashboard');
+            } else {
+                return redirect()->to('/login');
+            }
+        } else {
+            return redirect()->to('/login');
+        }
+        return redirect()->to('/login');
+    }
+}
